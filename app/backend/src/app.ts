1759dcb4192f36ddb72a2df8e7111d7198ac51cc
matheus-luz <luz.matheus11@gmail.com>
2022-11-04 15:08:18 -0300

    chore: eslint

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import * as express from 'express';
 
 class App {
   public app: express.Express;
@@ -8,21 +8,22 @@ class App {
 
     this.config();
 
-    // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
   }
 
   private config():void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header(
+        'Access-Control-Allow-Methods', 
+        'GET,POST,DELETE,OPTIONS,PUT,PATCH',
+      );
       res.header('Access-Control-Allow-Headers', '*');
       next();
     };
 
     this.app.use(express.json());
     this.app.use(accessControl);
-
   }
 
   public start(PORT: string | number):void {
@@ -32,5 +33,4 @@ class App {
 
 export { App };
 
-// A execução dos testes de cobertura depende dessa exportação
 export const { app } = new App();
--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,4 +1,5 @@
 import { App } from './app';
+
 import 'dotenv/config';
 
 const PORT = process.env.APP_PORT || 3001;
