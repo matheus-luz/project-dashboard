a2bc72db383cf06c6fe02f0b649a025c9c94521a
matheus-luz <luz.matheus11@gmail.com>
2022-11-05 18:46:02 -0300

    chore: eslint config

--- app/frontend/src/pages/Login.jsx
+++ app/frontend/src/pages/Login.jsx
@@ -1,8 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+import { requestLogin, setToken, requestData } from '../services/api';
 
 const Login = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [isLogged, setIsLogged] = useState(false);
+  const [failedTryLogin, setFailedTryLogin] = useState(false);
+
+  const login = async (event) => {
+    event.preventDefault()
+
+    try {
+      const { token } = await requestLogin('/login', { name, email });
+
+      setToken(token);
+
+      const { last_name } = await requestData('/login/validate', { name, email });
+
+      localStorage.setItem('token',  token);
+      localStorage.setItem('last_name',  last_name);
+
+      setIsLogged(true);
+    } catch (error) {
+      setFailedTryLogin(true);
+      setIsLogged(false);
+    }
+  };
+
+  useEffect(() => {
+    setFailedTryLogin(false);
+  }, [name, email]);
+
+  if (isLogged) return <Navigate to="/matches" />;
 
   return (
     <>
@@ -26,8 +56,21 @@ const Login = () => {
               placeholder="Email"
             />
           </label>
+          {
+            (failedTryLogin)
+              ? (
+                <p>
+                  {
+                    `O endereço de e-mail não está correto.
+                    Por favor, tente novamente.`
+                  }
+                </p>
+              )
+              : null
+          }
           <button
             type="submit"
+            onClick={ (event) => login(event) }
           >
             Entrar
           </button>
