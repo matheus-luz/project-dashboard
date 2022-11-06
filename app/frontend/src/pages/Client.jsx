e5b81f8ff2bf05790a9e528545499a2ad125f50f
matheus-luz <luz.matheus11@gmail.com>
2022-11-06 14:38:58 -0300

    feat: client route

--- app/frontend/src/pages/Client.jsx
+++ app/frontend/src/pages/Client.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Loading from '../components/Loading';
+import { requestData, setToken } from '../services/api';
 
 const Client = () => {
+  const [client, setClient] = useState([]);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      const token = localStorage.getItem('token') || '';
+
+      if (!token) return navigate('/');
+
+      setToken(token);
+
+      requestData('/login/validate')
+        .then(() => setIsAuthenticated(true))
+        .catch(() => navigate('/'));
+    })();
+  }, [navigate]);
+
+  useEffect(() => {
+    const endpoint = '/client';
+
+    const token = localStorage.getItem('token') || '';
+    if (token !== '') {
+      setToken(token);
+    }
+    if (!client.length) {
+      requestData(endpoint)
+        .then((response) => {
+          setClient(response);
+        })
+        .catch((error) => console.log(error));
+    }
+  });
+
+
+  if (!isAuthenticated) return <Loading />;
 
   return (
     <>
-      <section>
-        <h1>Hello client</h1>
-      </section>
     </>
   );
 };
