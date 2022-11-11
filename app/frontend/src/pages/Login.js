4bbd995d13f8140abeb4aa001473b525f6b8ed58
matheus-luz <luz.matheus11@gmail.com>
2022-11-11 00:43:24 -0300

    implement socket

--- app/frontend/src/pages/Login.js
+++ app/frontend/src/pages/Login.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { requestLogin, setToken } from '../services/api';
 
+import io from 'socket.io-client';
+import uuid from 'uuid';
+
+const myId = uuid()
+const socket = io('http://localhost:3000')
+socket.on('connect', () => console.log('[IO] Connect => A new connection has been established'))
+
 import '../styles/pages/login.css';
 
 const Login = () => {
@@ -10,8 +17,20 @@ const Login = () => {
   const [isLogged, setIsLogged] = useState(false);
   const [failedTryLogin, setFailedTryLogin] = useState(false);
 
+  const [message, updateMessage] = useState('')
+  const [messages, updateMessages] = useState([])
+
   const login = async (event) => {
     event.preventDefault()
+
+    if (message.trim()) {
+      socket.emit('chat.message', {
+          id: myId,
+          message
+      })
+      updateMessage('')
+      console.log(1);
+  }
 
     try {
       const { token } = await requestLogin('/login', { email, password });
@@ -26,6 +45,14 @@ const Login = () => {
       setIsLogged(false);
     }
   };
+
+  useEffect(() => {
+    const handleNewMessage = newMessage =>
+            updateMessages([...messages, newMessage])
+        socket.on('chat.message', handleNewMessage)
+        return () => socket.off('chat.message', handleNewMessage)
+
+    }, [])
 
   useEffect(() => {
     setFailedTryLogin(false);
